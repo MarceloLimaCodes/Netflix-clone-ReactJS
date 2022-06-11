2be82bebe332b33e74fa6f8b8ff7a0d9398ec033
MarceloLimaCodes <marcelolima557@gmail.com>
2022-06-11 03:12:15 -0300

    Animação de carregamento

--- src/App.js
+++ src/App.js
@@ -66,6 +66,12 @@ export default () => {
         Dados pegos do site themoviedb.org <br/>
         Feito por MarceloLimaCodes
       </footer>
+      
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src='https://i.gifer.com/origin/36/36527397c208b977fa3ef21f68c0f7b2_w200.gif' alt='Carregando' />
+        </div>
+      }
     </div>
   );
 }
